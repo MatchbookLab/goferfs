e89c53fe12fbee0826f4b1abc96af589cc8884d6
Cameron Spear <cameronspear@gmail.com>
2017-01-04 22:18:47 -0800

    Rename interfaces to a little better structure

--- src/suite.test.ts
+++ src/suite.test.ts
@@ -1,9 +1,11 @@
 import * as chai from 'chai';
+import * as chaiAsPromised from 'chai-as-promised';
 
 import Filesystem from './filesystem';
 import { IAdapter } from './types';
 import { Readable } from 'stream';
 
+chai.use(chaiAsPromised);
 chai.should();
 
 export default function(adapter: IAdapter) {
@@ -35,6 +37,21 @@ export default function(adapter: IAdapter) {
             await filesystem.deleteDir('path');
 
             (await filesystem.exists('path/to/test.txt')).should.equal(false);
+        });
+
+        it('should throw if you try to create a file that exists', async () => {
+            await filesystem.write('path/to/test.txt', 'Test');
+
+            return filesystem.create('path/to/test.txt', 'Test').should.eventually.be.rejectedWith('Cannot create a file that already exists. Use [create] or [update]');
+        });
+
+        it('should throw if you try to update a file that does not exists', async () => {
+            await filesystem.update('path/to/test.txt', 'Test Again');
+
+            const { contents } = await filesystem.read('path/to/test.txt');
+            contents.should.equal('Test Again');
+
+            return filesystem.update('path/to/does-not-exist.txt', 'Test').should.eventually.be.rejectedWith('Cannot update a file that does not exists. Use [create] or [write]');
         });
     });
 
@@ -81,8 +98,8 @@ export default function(adapter: IAdapter) {
             file.contents.should.equal('Test');
             file.name.should.equal('test1.txt');
             file.ext.should.equal('.txt');
-            file.path.should.match(/path\/to\/test1\.txt$/);
-            file.parentDir.should.match(/path\/to$/);
+            file.path.should.equal('path/to/test1.txt');
+            file.parentDir.should.equal('path/to');
             file.size.should.equal(4);
             file.isFile.should.equal(true);
             file.isDir.should.equal(false);
@@ -91,12 +108,12 @@ export default function(adapter: IAdapter) {
         });
 
         it('should do streams', async () => {
-            const writeStream = new Readable();
+            const inputStream = new Readable();
 
-            writeStream.push('Test Stream');
-            writeStream.push(null);
+            inputStream.push('Test Stream');
+            inputStream.push(null);
 
-            await filesystem.writeStream('path/to/test1.txt', writeStream);
+            await filesystem.writeStream('path/to/test1.txt', inputStream);
 
             const { stream } = await filesystem.readStream('path/to/test1.txt');
 
@@ -108,6 +125,28 @@ export default function(adapter: IAdapter) {
             });
 
             contents.should.equal('Test Stream');
+        });
+
+        it('should throw if you try to createStream a file that exists', async () => {
+            await filesystem.write('path/to/test.txt', 'Test');
+
+            return filesystem.createStream('path/to/test.txt', new Readable()).should.eventually.be.rejectedWith('Cannot create a file that already exists. Use [create] or [update]');
+        });
+
+        it('should throw if you try to updateStream a file that does not exists', async () => {
+            await filesystem.write('path/to/test.txt', 'Test');
+
+            const inputStream = new Readable();
+
+            inputStream.push('Test Again');
+            inputStream.push(null);
+
+            await filesystem.updateStream('path/to/test.txt', inputStream);
+
+            const { contents } = await filesystem.read('path/to/test.txt');
+            contents.should.equal('Test Again');
+
+            return filesystem.updateStream('path/to/does-not-exist.txt', new Readable()).should.eventually.be.rejectedWith('Cannot update a file that does not exists. Use [create] or [write]');
         });
     });
 }
