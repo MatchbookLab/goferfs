5510e92977bde670e44abad75eed4b7409b6d84b
Cameron Spear <cameronspear@gmail.com>
2017-01-24 18:01:44 -0800

    publish script attempt 1

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,3 +1,27 @@
+#!/usr/bin/env bash
+
+set -e
+
 np $1
-(cd interfaces && np $1)
-(cd types && np $1)
+
+version=`grep version package.json | cut -c 15- | rev | cut -c 3- | rev`
+
+pushd interfaces
+
+interfaces_version=`grep version package.json | cut -c 15- | rev | cut -c 3- | rev`
+
+sed -i '' "s/\"version\": \"{$interfaces_version}\"/\"version\": \"${version}\"/" package.json
+
+npm publish
+
+popd
+
+pushd types
+
+types_version=`grep version package.json | cut -c 15- | rev | cut -c 3- | rev`
+
+sed -i '' "s/\"version\": \"{$types_version}\"/\"version\": \"${version}\"/" package.json
+
+npm publish
+
+popd
